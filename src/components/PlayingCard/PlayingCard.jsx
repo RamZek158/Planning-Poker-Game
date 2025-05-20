28224eb914a49391bb18cb8f89b9d702a97178a8
Ruslan Makarov <rmakarov@sprinterra.ru>
2025-05-20 15:33:07 +0300

    Fitst  commit

---
+++ src/components/PlayingCard/PlayingCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const PlayingCard = () => {
+
+    return (
+        <div className="playingCard">
+
+        </div>
+    )
+}
+
+PlayingCard.displayName = 'PlayingCard';
+export default PlayingCard;
+
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
-export { CreateGame } from './CreateGame'
-export { Header } from './Header'
-export { HomePage } from './HomePage'
+export { CreateGame } from './CreateGame';
+export { Header } from './Header';
+export { HomePage } from './HomePage';
+export { PlayingCard } from './PlayingCard';
