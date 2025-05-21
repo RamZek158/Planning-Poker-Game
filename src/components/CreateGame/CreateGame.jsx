2c0574b95afa53961871b353736f712a3cf9aaf6
RamZek158 <extaxdead@gmail.com>
2025-05-21 10:19:18 +0300

    added disablet for button

--- src/components/CreateGame/CreateGame.jsx
+++ src/components/CreateGame/CreateGame.jsx
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
-import './CreateGame.css'; // Стили для траницы
-import { T_SHIRT_VOTING_SYSTEM, FIBONACCI_VOTING_SYSTEM } from '../../utils'
+import React from 'react';
+import './CreateGame.css';
+import { T_SHIRT_VOTING_SYSTEM, FIBONACCI_VOTING_SYSTEM } from '../../utils';
+
 const CreateGame = () => {
   const t_shirt_system_string = T_SHIRT_VOTING_SYSTEM.join(', ') ;
   const fibonacci_system_string = FIBONACCI_VOTING_SYSTEM.join(', ') ;
+
+  const handleChangeGameName = React.useCallback((event) => {
+     console.log('change', event.target.value)
+  }, []);
 
   return (
     <div className="create-game-container">
       <section>
         <div id="InputName">
           <input type="text" 
+          onChange={handleChangeGameName}
                  id="InputName" 
                  placeholder="Название игры" 
           />
@@ -19,7 +25,7 @@ const CreateGame = () => {
           <option>{`T-shirts (${t_shirt_system_string})`}</option>
         </select>
           <div>
-            <button className="NewGame">Начать игру</button>
+            <button className="NewGame" disabled>Начать игру</button>
           </div>
       </section>
     
