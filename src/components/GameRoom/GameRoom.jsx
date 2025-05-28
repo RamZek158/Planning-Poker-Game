efcc94354439f31abef76140432c301c9c7dce05
RamZek158 <extaxdead@gmail.com>
2025-05-28 17:44:29 +0300

    added cards for game room

--- src/components/GameRoom/GameRoom.jsx
+++ src/components/GameRoom/GameRoom.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PlayingCard } from '../../components';
 import { useParams } from 'react-router';
 
 const GameRoom = () => {
@@ -8,10 +9,26 @@ const GameRoom = () => {
 
 
   return (
-    <div>
-      <h1>Игра с ID</h1>
-      <p>Добро пожаловать</p>
-    </div>
+    <section className="hero pageContainer">
+         <div>
+            <h1>Игра с ID</h1>
+            <p>Добро пожаловать</p>
+        </div>
+      <div style={{display: 'flex', margin: '10px'}}>
+            <div style={{margin: '10px', padding: '10px'}}>
+                <PlayingCard randomCardSuit={0} cardValue="L" />
+            </div>
+            <div style={{margin: '10px', padding: '10px'}}>
+                <PlayingCard randomCardSuit={1} cardValue="XL" />
+            </div>
+            <div style={{margin: '10px', padding: '10px'}}>
+                <PlayingCard randomCardSuit={2} cardValue="XXL" />
+            </div>
+            <div style={{margin: '10px', padding: '10px'}}>
+                <PlayingCard randomCardSuit={3} cardValue="?" />
+            </div>
+      </div>
+    </section>
   );
 };
 
