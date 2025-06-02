aa22157f7c5fe9eac8a1d5bc2178f07f6efa5beb
Ruslan Makarov <rmakarov@sprinterra.ru>
2025-06-02 15:58:27 +0300

    Merge from remote branch

--- mock-api.js
+++ mock-api.js
@@ -29,11 +29,11 @@ router.delete ('/users', (req, res) => {
         const usersFromFile = JSON.parse(fs.readFileSync('./tmp/users.json', 'utf8'));
         const updatedUsers = usersFromFile.filter(user => user.id !== userId);
 
-        console.log('userId: ', userId);
+       // console.log('userId: ', userId);
         let jsonString = JSON.stringify(updatedUsers, null, 4);
         fs.writeFileSync('./tmp/users.json', jsonString);
 
-        console.log('updatedUsers: ', updatedUsers);
+        // console.log('updatedUsers: ', updatedUsers);
         res.json(updatedUsers);
     }
     res.status(500).send('Internal Server Error');
